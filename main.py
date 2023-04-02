bc980520535ba3195ef8e79e6f110c10cdd44b9a
Kaspar Snashall <kasparsnashall@yahoo.co.uk>
2023-04-02 16:12:25 +0100

    Started adding a bar indicator for sail line

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from numpy import sin, cos
 import arcade # using this for the game
 import pandas as pd 
 from scipy.interpolate import interp1d
+#from Indicator import IndicatorBar
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -29,18 +30,22 @@ WINDSPEED = 2
 WINDANGLE = 360
 WINDVECTOR = [WINDSPEED*cos(np.deg2rad(WINDANGLE)), WINDSPEED*sin(np.deg2rad(WINDANGLE))]
 
+
 class Boat(arcade.Sprite):
     """
     Player class
     """
-    sail_angle = 180 # angle of the sail 
-    boat_data = pd.read_csv('speed.csv')
-    interp_velocity = interp1d(boat_data['angle'],boat_data['max velocity (wind fraction)'])
-    interp_liftcoeff = interp1d(boat_data['angle'],boat_data['lift coeff'])
-    change_sail_angle = 0
-    accel = 0
-
-    
+    def __init__(self, filename, scale, bar_list: arcade.SpriteList):
+        super().__init__(filename=filename,scale=scale)
+        self.sail_angle = 180 # angle of the sail 
+        self.boat_data = pd.read_csv('speed.csv')
+        self.interp_velocity = interp1d(self.boat_data['angle'],self.boat_data['max velocity (wind fraction)'])
+        self.interp_liftcoeff = interp1d(self.boat_data['angle'],self.boat_data['lift coeff'])
+        self.change_sail_angle = 0
+        self.accel = 0
+        self.indicator_bar: Indicatorbar = IndicatorBar(
+            self, bar_list, (self.center_x, self.center_y)
+            )
 
     def update(self):
         # Move player.
@@ -52,11 +57,9 @@ class Boat(arcade.Sprite):
         if self.sail_angle > 90: # relative to the boat direction
             self.sail_angle = 90
 
-
-        
-
-        self.velocity = WINDSPEED*(self.interp_velocity(abs(self.angle)%360)) + self.accel
+        self.velocity = WINDSPEED*(self.interp_velocity(abs(self.angle)%360)) + self.accel 
         self.move_vector = [self.velocity*cos(np.deg2rad(self.angle)), self.velocity*sin(np.deg2rad(self.angle))]
+        
         # need to work out the apprent wind vector
         angle_attack = abs(self.angle)-self.sail_angle
         print(angle_attack)
@@ -124,7 +127,7 @@ class SailGame(arcade.Window):
         """ Set up the game variables. Call to re-start the game. """
         # Create your sprites and sprite lists here
         self.player_list = arcade.SpriteList()
-        self.player_sprite = Boat("Boat.png", 0.05)
+        self.player_sprite = Boat("Boat.png", 0.05, self.player_list)
         self.player_sprite.center_x = self.position[0]
         self.player_sprite.center_y = self.position[1]
         self.player_list.append(self.player_sprite)
@@ -206,6 +209,120 @@ class SailGame(arcade.Window):
         Called when a user releases a mouse button.
         """
         pass
+
+
+class IndicatorBar:
+    """
+    Represents a bar which can display information about a sprite.
+
+    :param Player owner: The owner of this indicator bar.
+    :param arcade.SpriteList sprite_list: The sprite list used to draw the indicator
+  1
+  1 initial position of the bar.
+    :param arcade.Color full_color: The color of the bar.
+    :param arcade.Color background_color: The background color of the bar.
+    :param int width: The width of the bar.
+    :param int height: The height of the bar.
+    :param int border_size: The size of the bar's border.
+    """
+
+    def __init__(
+        self,
+        owner: Boat,
+        sprite_list: arcade.SpriteList,
+        position: tuple[float, float] = (0, 0),
+        full_color: arcade.Color = arcade.color.GREEN,
+        background_color: arcade.Color = arcade.color.BLACK,
+        width: int = 100,
+        height: int = 4,
+        border_size: int = 4,
+    ) -> None:
+        # Store the reference to the owner and the sprite list
+        self.owner: Player = owner
+        self.sprite_list: arcade.SpriteList = sprite_list
+
+        # Set the needed size variables
+        self._box_width: int = width
+        self._box_height: int = height
+        self._half_box_width: int = self._box_width // 2
+        self._center_x: float = 0.0
+        self._center_y: float = 0.0
+        self._fullness: float = 0.0
+
+        # Create the boxes needed to represent the indicator bar
+        self._background_box: arcade.SpriteSolidColor = arcade.SpriteSolidColor(
+            self._box_width + border_size,
+            self._box_height + border_size,
+            background_color,
+        )
+        self._full_box: arcade.SpriteSolidColor = arcade.SpriteSolidColor(
+            self._box_width,
+            self._box_height,
+            full_color,
+        )
+        self.sprite_list.append(self._background_box)
+        self.sprite_list.append(self._full_box)
+
+        # Set the fullness and position of the bar
+        self.fullness: float = 1.0
+        self.position: tuple[float, float] = position
+
+    def __repr__(self) -> str:
+        return f"<IndicatorBar (Owner={self.owner})>"
+
+    @property
+    def background_box(self) -> arcade.SpriteSolidColor:
+        """Returns the background box of the indicator bar."""
+        return self._background_box
+
+    @property
+    def full_box(self) -> arcade.SpriteSolidColor:
+        """Returns the full box of the indicator bar."""
+        return self._full_box
+
+    @property
+    def fullness(self) -> float:
+        """Returns the fullness of the bar."""
+        return self._fullness
+
+    @fullness.setter
+    def fullness(self, new_fullness: float) -> None:
+        """Sets the fullness of the bar."""
+        # Check if new_fullness if valid
+        if not (0.0 <= new_fullness <= 1.0):
+            raise ValueError(
+                f"Got {new_fullness}, but fullness must be between 0.0 and 1.0."
+            )
+
+        # Set the size of the bar
+        self._fullness = new_fullness
+        if new_fullness == 0.0:
+            # Set the full_box to not be visible since it is not full anymore
+            self.full_box.visible = False
+        else:
+            # Set the full_box to be visible incase it wasn't then update the bar
+            self.full_box.visible = True
+            self.full_box.width = self._box_width * new_fullness
+            self.full_box.left = self._center_x - (self._box_width // 2)
+
+    @property
+    def position(self) -> tuple[float, float]:
+        """Returns the current position of the bar."""
+        return self._center_x, self._center_y
+
+    @position.setter
+    def position(self, new_position: tuple[float, float]) -> None:
+        """Sets the new position of the bar."""
+        # Check if the position has changed. If so, change the bar's position
+        if new_position != self.position:
+            self._center_x, self._center_y = new_position
+            self.background_box.position = new_position
+            self.full_box.position = new_position
+
+            # Make sure full_box is to the left of the bar instead of the middle
+            self.full_box.left = self._center_x - (self._box_width // 2)
+
+
 
 
 if __name__ == "__main__":
